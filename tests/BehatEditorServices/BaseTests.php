64d8deee0867c1aa96664b7cf26e8d4f3188cb33
alnutile <alfrednutile@gmail.com>
2014-03-06 13:00:58 +0000

    Get Route to Controllers tested fully

--- tests/BehatEditorServices/BaseTests.php
+++ tests/BehatEditorServices/BaseTests.php
@@ -1,14 +1,29 @@
 <?php namespace BehatEditorServices;
 
+use Mockery;
+
 class BaseTests extends \PHPUnit_Framework_TestCase {
+
+    public $sitesController;
+    public $testsController;
+    public $reportsController;
+    public $batchesController;
+    public $tagsController;
+    public $tokensController;
 
 
     function setUp()
     {
       parent::setUp();
+      $this->sitesController              = Mockery::mock('BehatEditorSitesController');
+      $this->testsController              = Mockery::mock('BehatEditorTestsController');
+      $this->reportsController            = Mockery::mock('BehatEditorReportsController');
+      $this->batchesController            = Mockery::mock('BehatEditorBatchesController');
+      $this->tagsController               = Mockery::mock('BehatEditorTagsController');
+      $this->tokensController             = Mockery::mock('BehatEditorTokensController');
     }
 
-    function testSetup()
+    function testGo()
     {
 
     }
